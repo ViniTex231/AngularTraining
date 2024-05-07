501f94404c741cedd493eaebdfdbe451e87a4dd3
ViniTex231 <viniteixeira2828@gmail.com>
2024-05-07 13:02:38 -0300

    feat: services created, but with error

--- FrontEnd/smartfit/src/app/app.component.ts
+++ FrontEnd/smartfit/src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormsComponent, ReactiveFormsModule],
+  imports: [RouterOutlet, HeaderComponent, FormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'smartfit';
 }
+
+
